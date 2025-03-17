7a489f0478eeab4fe199b36e41b1b62d86d9c56d
EggCox <40557922+EdToxer@users.noreply.github.com>
2025-03-17 17:58:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os, glob, time, sys, pygame
 from PySide6.QtWidgets import QApplication, QMainWindow
-from ui_main import Ui_MainWindow
-from threading import Thread
+from music_gui_1 import Ui_MainWindow
+import threading
 import math
 import random
+
 
 class MainWindow(QMainWindow):
     end = False
@@ -13,55 +14,57 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
         self.setWindowTitle("Music Player")
 
+        self.t1 = threading.Thread(target=self.progress)
+
         self.music_loop = False
-        self.path = "C:\\Users\\INSERT-YOUR-USER\\Music\\"
+        self.path = "C:\\Users\\keshu\\Music\\"
         self.random = False
         self.repeat = False
         self.first_track = True
         #Play/stop
-        self.ui.pushButton_3.clicked.connect(self.play_stop_track)
+        self.ui.push_Play_Stop.clicked.connect(self.play_stop_track)
         #Previous
-        self.ui.pushButton_2.clicked.connect(self.previous_track)
+        self.ui.push_Back.clicked.connect(self.previous_track)
         #Next
-        self.ui.pushButton.clicked.connect(self.next_track)
-        
-        self.ui.verticalSlider.sliderMoved.connect(self.update_volume)
+        self.ui.push_Next.clicked.connect(self.next_track)
+        #Music_volume
+        self.ui.slider_Volume.sliderMoved.connect(self.update_volume)
         #Random
-        self.ui.checkBox.clicked.connect(self.update_random)
-        
-        self.ui.checkBox_2.clicked.connect(self.update_repeat)
+        self.ui.push_Shuffle.clicked.connect(self.update_random)
+        #Repeat
+        self.ui.push_Repeat.clicked.connect(self.update_repeat)
         
         self.music_collection = []
         self.music_shuffled = []
         pygame.init()
         
-        pygame.mixer.music.set_volume(1)
+        pygame.mixer.music.set_volume(1.0)
         self.track_next = 0
         self.load_path()
         self.load_track()
         self.threading()
 
-        
+
     def update_volume(self):
-        slider_value = self.ui.verticalSlider.value(0)
+        slider_value = self.ui.slider_Volume.value()
         music_volume = (slider_value - slider_value % 10)  / 10
         pygame.mixer.music.set_volume(music_volume)
-        print(music_volume)
+
     def load_path(self):
+        self.ui.slider_Volume.setValue(0)
         for filename in glob.glob(os.path.join(self.path, '*.mp3')):
             with open(os.path.join(os.getcwd(), filename), 'r') as f:
                 self.music_collection.append(f.name.replace(self.path,""))
         self.music_shuffled = list(self.music_collection)
         random.shuffle(self.music_shuffled)
 
-
     def update_labels(self, music_collection):
         fullname = music_collection[self.track_next].replace(".mp3","")
         pos = fullname.rfind("-")
         author_name = fullname[0:pos]
         track_name = fullname.replace(author_name,"").replace("-","")
-        self.ui.label_3.setText(author_name)
-        self.ui.label_2.setText(track_name)
+        self.ui.label_Author.setText(author_name)
+        self.ui.label_TrackName.setText(track_name)
 
     def update_repeat(self):
         if self.repeat == True:
@@ -77,11 +80,10 @@ class MainWindow(QMainWindow):
 
     def update_progress_bar(self, track_name):
         song_len = pygame.mixer.Sound(track_name).get_length()
-        self.ui.progressBar.setMinimum(0)
-        self.ui.progressBar.setMaximum(math.ceil(song_len))
+        self.ui.bar_Music.setMinimum(0)
+        self.ui.bar_Music.setMaximum(math.ceil(song_len))
 
     def load_track(self):
-        print(self.track_next)
         if self.random == True:
             track_name = self.path + self.music_shuffled[self.track_next]
             pygame.mixer.music.load(track_name, "mp3")
@@ -128,31 +130,29 @@ class MainWindow(QMainWindow):
         self.music_loop = True
 
     def progress(self):
-        while not self.end:
+        t1 = threading.current_thread()
+        while getattr(t1, "do_run", True):
             if self.music_loop == True:
                 if pygame.mixer.music.get_pos() != (-1):
-                    self.ui.progressBar.setValue(pygame.mixer.music.get_pos()/ 1000)
+                    self.ui.bar_Music.setValue(pygame.mixer.music.get_pos()/ 1000)
                 elif pygame.mixer.music.get_pos() == (-1) and self.repeat == True:
                     pygame.mixer.music.play()
                 elif pygame.mixer.music.get_pos() == (-1):
                     self.next_track()
                     pygame.mixer.music.play()
             time.sleep(.4)
-        if self.end == True:
-            pygame.quit()
-    def threading(self):
-        t1 = Thread(target=self.progress)
-        t1.start()
 
-def game_quit():
-    MainWindow.end = True
+    def threading(self):
+        self.t1.start()
+
+def game_quit(wind):
+    wind.t1.do_run = False
+    pygame.quit()
     sys.exit()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    
     window = MainWindow()
     window.show()
-
     if app.exec() == 0:
-        game_quit()
+        game_quit(window)
